90cffd19b6d39c6b3c267e2fc7f6b09357e6191f
belafrata <fratabela@gmail.com>
2026-03-06 10:38:11 -0300

    Criando repositório

---
+++ PrimeiroApp/Repository/Contract/IUsuarioRepository.cs
@@ -0,0 +1,21 @@
+﻿using PrimeiroApp.Models;
+
+namespace PrimeiroApp.Repository.Contract
+{
+    public interface IUsuarioRepository
+    {
+        // CRUD
+
+        IEnumerable<Usuario> ObterTodosUsuarios();
+
+        void Cadastrar(Usuario usuario);
+
+        void Atualizar(Usuario usuario);
+
+        Usuario ObterUsuario(int Id);
+
+        void Excluir(int Id);
+
+
+    }
+}
